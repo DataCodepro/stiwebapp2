92a9e4547d9ab23490554118a588b95e89e52a1c
Simeon-paul Taiwo Emmanuel <65008596+Emmanuel2202@users.noreply.github.com>
2022-06-23 02:59:39 -0700

    Update labelsfortablereport.py

--- pages/labelsfortablereport.py
+++ pages/labelsfortablereport.py
@@ -4,14 +4,23 @@ def label():
     if select == 'TABLE':
         st.subheader('TABLE REPORT LABEL FOR TABLE 3')
         st.write('''sinfo1:Information source - Internal
+        
  sinfo2:Information source - Suppliers
+ 
  sinfo3:Information source - Customers
+ 
  sinfo4:Information source - Competitors
+ 
  sinfo5:Information source - Consultants, commercial labs or private R&D institutes
+ 
  sinfo6:Information source - Universities, other higher ed. Institutions
+ 
  sinfo7:Information source - Public research institutes
+ 
  sinfo8:Information source - Conferences, fairs, exhibitions
+ 
  sinfo9:Information source - Journals, trade publications
+ 
  sinfo10:Information source - Professional, industry associations
     ''')
         if st.checkbox('TABLE REPORT LABEL FOR TABLE 4'):
